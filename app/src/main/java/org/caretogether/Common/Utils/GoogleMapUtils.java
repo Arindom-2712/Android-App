4d5c18cf525b6a05172ba7121c8a977927f86d84
unknown <saikiaarindam@gmail.com>
2018-09-29 14:26:18 +0530

    [Getting Route/Steps and Legs from MAP - outline code]

--- app/src/main/java/org/caretogether/Common/Utils/GoogleMapUtils.java
+++ app/src/main/java/org/caretogether/Common/Utils/GoogleMapUtils.java
@@ -1,8 +1,30 @@
 package org.caretogether.Common.Utils;
 
+import android.graphics.Color;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import org.caretogether.Consumer.ConsumerUtils;
 import org.caretogether.ServiceProvider.ServiceProviderUtils.WayPoints;
 import org.caretogether.ServiceProvider.ServiceProviderUtils.AmbulanceLocation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
+
 
 /**
  * Created by Admin on 9/29/2018.
@@ -10,15 +32,99 @@ import org.caretogether.ServiceProvider.ServiceProviderUtils.AmbulanceLocation;
 
 public class GoogleMapUtils {
 
-    public static WayPoints[] getWaypoints( AmbulanceLocation src, AmbulanceLocation dst) {
+    private String TAG = "GoogleMapUtils";
+    private static GoogleMap mMap;
 
+    public static WayPoints[] getWaypoints( GoogleMap gMap, LatLng ltlngSRC, LatLng ltlngDEST) {
+
+        mMap = gMap;
+        LatLng ambulanceRouteSRC = ltlngSRC;
+        LatLng ambulanceRouteDES = ltlngDEST;
+        
         WayPoints[] lstWayPoints = new WayPoints[]{};
+
         //TODO
+
 
         return lstWayPoints;
     }
 
     public static ConsumerUtils.CommuterInfo getCommuterInfo() {
         return null;
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng barcelona = new LatLng(41.385064,2.173403);
+        mMap.addMarker(new MarkerOptions().position(barcelona).title("Marker in Barcelona"));
+
+        LatLng madrid = new LatLng(40.416775,-3.70379);
+        mMap.addMarker(new MarkerOptions().position(madrid).title("Marker in Madrid"));
+
+        LatLng zaragoza = new LatLng(41.648823,-0.889085);
+
+        //Define list to get all latlng for the route
+        List<LatLng> path = new ArrayList();
+
+
+        //Execute Directions API request
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("YOUR_API_KEY")
+                .build();
+        DirectionsApiRequest req = DirectionsApi.getDirections(context, "41.385064,2.173403", "40.416775,-3.70379");
+        try {
+            DirectionsResult res = req.await();
+
+            //Loop through legs and steps to get encoded polylines of each step
+            if (res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+
+                if (route.legs !=null) {
+                    for(int i=0; i<route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        if (leg.steps != null) {
+                            for (int j=0; j<leg.steps.length;j++){
+                                DirectionsStep step = leg.steps[j];
+                                if (step.steps != null && step.steps.length >0) {
+                                    for (int k=0; k<step.steps.length;k++){
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+                                        if (points1 != null) {
+                                            //Decode polyline and add points to list of route coordinates
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if (points != null) {
+                                        //Decode polyline and add points to list of route coordinates
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch(Exception ex) {
+            Log.e(TAG, ex.getLocalizedMessage());
+        }
+
+        //Draw the polyline
+        if (path.size() > 0) {
+            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+            mMap.addPolyline(opts);
+        }
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zaragoza, 6));
     }
 }
